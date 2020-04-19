511332b44c0a45956109d47075b3f715e299484b
최영훈 <c_yh0327@naver.com>
2020-04-20 00:47:04 +0900

    feat/CRUD memeber

--- homework1/group.js
+++ homework1/group.js
@@ -21,11 +21,45 @@ var group2 ={
     },
     addMember: function(name,nickname,age,status){
         const str = '{"name":"'+name+'", "nickname":"'+nickname+'", "age":'+age+', "status":"'+status+'"}';
-        this.members.push(JSON.parse(str))
+        const newMemberObj = JSON.parse(str)
+        this.members.push(newMemberObj)
+        console.log('멤버 생성 완료!',newMemberObj)
+    },
+    deleteMember: function(name){
+        const idx = this.findMemberIndex(name);
+        if(idx > -1){
+            console.log('멤버 삭제 완료', this.members[idx])
+            this.members.splice(idx,1);   
+        }
+    },
+    updateMember: function(name, nickname, age, status){
+        const idx = this.findMemberIndex(name);
+        if(idx > -1){
+            const str = '{"name":"'+name+'", "nickname":"'+nickname+'", "age":'+age+', "status":"'+status+'"}';
+            this.members[idx] = JSON.parse(str);
+            console.log('업데이트 완료!', this.members[idx]);
+        }
+    },
+    findMemberIndex: function(name){
+        const nameToFind = this.members.find(function(item){
+            return item.name === name;
+        })
+        const idx = this.members.indexOf(nameToFind)
+        return idx;
     }
 }
 console.log(group2);
+console.log('-----Create------')
+group2.addMember('박경선',"서버요정",2,'OB');
+
+console.log('-----Read------')
 group2.printGroupName();
 group2.printAllMember();
-group2.addMember('박경선',"서버요정",2,'파트장');
+
+console.log('-----Update------')
+group2.updateMember('박경선',"서버파트장",23,"서버 파트장")
+
+console.log('-----Delete------')
+group2.deleteMember('최영훈');
+console.log('----Result------')
 console.log(group2);
