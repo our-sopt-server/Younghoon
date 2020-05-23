27c050f1b439aa6e92e3297644365a7bf1282f65
최영훈 <c_yh0327@naver.com>
2020-05-23 19:25:22 +0900

    uer Controller

---
+++ homework4/controller/userController.js
@@ -0,0 +1,81 @@
+const encryption = require('../modules/encryption');
+const users = require('../models/users');
+const util = require('../modules/util');
+const resMessage = require('../modules/responseMessage');
+const statusCode = require('../modules/statusCode');
+
+module.exports = {
+    signup: async (req, res) => {
+        const {
+            id,
+            name,
+            password,
+            email
+        } = req.body;
+        if (!id || !name || !password || !email) {
+            return await res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, resMessage.NULL_VALUE));
+        }
+
+        const idCheck = await users.checkUser(id);
+        if (idCheck) {
+            return await res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, resMessage.ALREADY_ID));
+        }
+
+        const salt = encryption.salt();
+        const idx = await users.signup(id, name, password, salt, email);
+        if (idx === -1) {
+            return await res.status(statusCode.DB_ERROR).send(util.fail(statusCode.DB_ERROR, resMessage.DB_ERROR));
+        }
+
+        return await res.status(statusCode.OK).send(util.success(statusCode.OK, resMessage.CREATED_USER, {
+            userId: id
+        }));
+    },
+    signin: async (req, res) => {
+        //로그인에 대한 로직
+        const {
+            id,
+            password
+        } = req.body;
+        if (!id || !password) {
+            return await res.status(statusCode.BAD_REQUEST)
+                .send(util.fail(statusCode.BAD_REQUEST, resMessage.NULL_VALUE));
+        }
+        // id 체크 확인
+        const idCheck = await users.checkUser(id);
+        if (!idCheck) {
+            return await res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, resMessage.NO_USER));
+        }
+
+        //로그인 로직
+        const result = await users.signin(id);
+        const DBPassword = result[0].password;
+        const DBSalt = result[0].salt;
+        const hashedPassword = await encryption.encrypt(password, DBSalt);
+
+        if (DBPassword !== hashedPassword) {
+            return await res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, resMessage.MISS_MATCH_PW));
+        }
+
+        await res.status(statusCode.OK).send(util.success(statusCode.OK, resMessage.LOGIN_SUCCESS, id));
+    },
+    getUserById: async (req, res) => {
+        //유저 조회하기 (유저 이메일, 이름 등등...)
+        const {
+            id
+        } = req.body;
+
+        //존재하는 아이디인지 확인 - 없다면 No user
+        const idCheck = await users.checkUser(id);
+        if (!idCheck) {
+            return await res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, resMessage.NO_USER));
+        }
+        const result = await users.getUserById(id);
+        const user = {
+            "id": result[0].id,
+            "name": result[0].name,
+            "email": result[0].email
+        };
+        return await res.status(statusCode.OK).send(util.success(statusCode.OK, resMessage.READ_PROFILE_SUCCESS, user));
+    }
+}
--- homework4/routes/users.js
+++ homework4/routes/users.js
@@ -1,76 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const encryption = require('../modules/encryption');
-const users = require('../models/users');
-const util = require('../modules/util');
-const resMessage = require('../modules/responseMessage');
-const statusCode = require('../modules/statusCode');
+const userController = require('../controller/userController')
 
-router.post('/signup', async (req,res) =>{
-  const {id, name, password, email} = req.body;
-
-  if(!id || !name || !password || !email){
-    return res.status(statusCode.BAD_REQUEST)
-    .send(util.fail(statusCode.BAD_REQUEST,resMessage.NULL_VALUE));
-  }
-  if(await users.checkUser(id)){
-    res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST,resMessage.ALREADY_ID));
-    return;
-  }
-
-  const salt = encryption.salt();
-  const idx = await users.signup(id,name,password,salt,email);
-  if(idx === -1){
-    return res.status(statusCode.DB_ERROR)
-    .send(util.fail(statusCode.DB_ERROR, resMessage.DB_ERROR));
-  }
-
-  res.status(statusCode.OK)
-  .send(util.success(statusCode.OK,resMessage.CREATED_USER,{userId:id}));
-});
-
-router.post('/signin', async(req,res) =>{
-  const {id, password} = req.body;
-
-  if(!id || !password){
-    return res.status(statusCode.BAD_REQUEST)
-    .send(util.fail(statusCode.BAD_REQUEST,resMessage.NULL_VALUE));
-  }
-  // id 체크 확인
-  const idCheck = await users.checkUser(id);
-  if(!idCheck){
-    res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST,resMessage.NO_USER));
-    return;
-  }
-  //로그인 로직
-  const result = await users.signin(id);
-  const DBPassword = result[0].password;
-  const DBSalt = result[0].salt;
-  const hashedPassword = await encryption.encrypt(password,DBSalt);
-
-  if(DBPassword !== hashedPassword){
-    return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST,resMessage.MISS_MATCH_PW));
-  }
-
-  res.status(statusCode.OK).send(util.success(statusCode.OK,resMessage.LOGIN_SUCCESS, id));  
-})
-
-router.get('/', function(req, res, next) {
-  res.send('user페이지');
-});
-
-router.post('/getUserById', async (req,res)=>{
-  //request params에서 데이터 가져오기
-  const {id} = req.body;
-  //존재하는 아이디인지 확인 - 없다면 No user
-  const idCheck = await users.checkUser(id);
-  if(!idCheck){
-    res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST,resMessage.NO_USER));
-    return;
-  }
-  const result = await users.getUserById(id);
-  const user = {"id":result[0].id, "name":result[0].name, "email":result[0].email};
-  res.status(statusCode.OK).send(util.success(statusCode.OK,resMessage.READ_PROFILE_SUCCESS,user));
-})
+router.post('/signup', userController.signup);
+router.post('/signin', userController.signin);
+router.post('/getUserById', userController.getUserById);
 
 module.exports = router;
